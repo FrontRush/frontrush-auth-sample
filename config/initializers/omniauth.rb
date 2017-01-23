2990fef24800560a1915645f62ae29a3d280eb42
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2017-01-23 15:34:30 -0500

    Updated dev url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
        #This points to the frontrush staging server
-	provider :frontrush, consumer_key: 'fake_key', consumer_secret: 'fake_secret', frontrush_url: 'http://frontrushtest.dyndns.org/FRConnectServices', setup: true
+	provider :frontrush, consumer_key: 'fake_key', consumer_secret: 'fake_secret', frontrush_url: ' http://frontrushrails.pleaserecruit.me/FRConnectServices', setup: true
 end	
 
