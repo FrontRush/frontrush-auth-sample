d61a62db25773decf8c8276e65234280acc7c601
dragon364 <dhanesh.n.m19@gmail.com>
2017-01-27 09:10:16 -0500

    url update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
        #This points to the frontrush staging server
-	provider :frontrush, consumer_key: 'fake_key', consumer_secret: 'fake_secret', frontrush_url: ' http://frontrushrails.pleaserecruit.me/FRConnectServices', setup: true
-end	
-
+	provider :frontrush, consumer_key: 'key3', consumer_secret: 'secret3', frontrush_url: 'http://frontrushrails.pleaserecruit.me/FRConnectServices', setup: true
+end
